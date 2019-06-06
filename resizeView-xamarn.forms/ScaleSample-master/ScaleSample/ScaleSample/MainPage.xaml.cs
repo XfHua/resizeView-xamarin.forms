f6a8264faf84b3c02a11634f246ffa91c7065cdd
XfHua <jasonHuaxf@163.com>
2019-06-06 13:17:19 +0800

    Add files via upload

---
+++ resizeView-xamarn.forms/ScaleSample-master/ScaleSample/ScaleSample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ScaleSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+
+    public class scaleView : View1 {
+
+        public scaleView(){
+
+            this.Scale = 0.5;
+        }
+    }
+}
